15a97c40dcd2abe0f9233c16684f75d62f5b05bd
Bibas Rai <130794180+bibasrairockz@users.noreply.github.com>
2024-03-29 02:28:21 +0000

    Add files via upload

---
+++ store_index.py
@@ -0,0 +1,33 @@
+from src.helper import load_pdf, text_split, download_hugging_face_embeddings
+from langchain.vectorstores import Pinecone
+import pinecone
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV')
+
+# print(PINECONE_API_KEY)
+# print(PINECONE_API_ENV)
+
+extracted_data = load_pdf("data/")
+text_chunks = text_split(extracted_data)
+embeddings = download_hugging_face_embeddings()
+
+
+#Initializing the Pinecone
+pc = Pinecone(api_key=PINECONE_API_KEY)
+
+
+
+index_name="starter-index"
+index = pc.Index(index_name)
+
+#Creating Embeddings for Each of The Text Chunks & storing
+from langchain.vectorstores import Pinecone
+text_field = "text"
+vectorstore = Pinecone(
+    index, embeddings.embed_query, text_field
+)
